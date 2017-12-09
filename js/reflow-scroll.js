99bdb1513eda446f56931d81d6c91bdc1a09cf15
Maxim Solodovnik <solomax666@gmail.com>
2017-12-09 13:14:23 +0700

    Site is updated due to 4.0.1 release

--- js/reflow-scroll.js
+++ js/reflow-scroll.js
@@ -1,7 +1,15 @@
-// Support for smooth scrolling
-// (simplified version, taken from http://stackoverflow.com/a/14805098/1173184)
+
 $(window).load(function(){
   $('a[href^="#"]:not([href^="#carousel"]):not([data-toggle="dropdown"])').on('click', function(e) {
+
+    function scrollTo(el, offset, clbck) {
+      var pos = (el && el.length > 0) ? el.offset().top : 0;
+      pos = pos + (offset ? offset : 0);
+
+      $('html,body').animate({
+          scrollTop: pos
+      }, 300, clbck);
+    };
 
      // prevent default anchor click behavior
      e.preventDefault();
@@ -9,15 +17,21 @@ $(window).load(function(){
      // store hash
      var hash = this.hash;
 
-     // animate
-     $('html, body').animate({
-         scrollTop: $(this.hash).offset().top
-       }, 300, function(){
-
-         // when done, add hash to url
-         // (default click behaviour)
-         window.location.hash = hash;
-       });
+    if (hash === "") {
+      // other
+      scrollTo();
+    } else {
+      // heading click
+      scrollTo(
+        $(this.hash),
+        undefined,
+        function(){
+          // when done, add hash to url
+          // (default click behaviour)
+          window.location.hash = hash;
+        }
+      );
+    }
 
   });
 });
