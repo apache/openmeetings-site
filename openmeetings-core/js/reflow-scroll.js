79f4738286e154815503fad413e41a246d7e8d85
Maxim Solodovnik <solomax666@gmail.com>
2018-02-25 18:07:33 +0700

    Site is updated due to 4.0.2 release

--- openmeetings-core/js/reflow-scroll.js
+++ openmeetings-core/js/reflow-scroll.js
@@ -1,6 +1,6 @@
 
 $(window).load(function(){
-  $('a[href^="#"]:not([href^="#carousel"]):not([data-toggle="dropdown"])').on('click', function(e) {
+  $('#m-scroll-top').on('click', function(e) {
 
     function scrollTo(el, offset, clbck) {
       var pos = (el && el.length > 0) ? el.offset().top : 0;
@@ -13,25 +13,8 @@ $(window).load(function(){
 
      // prevent default anchor click behavior
      e.preventDefault();
-
-     // store hash
-     var hash = this.hash;
-
-    if (hash === "") {
-      // other
-      scrollTo();
-    } else {
-      // heading click
-      scrollTo(
-        $(this.hash),
-        undefined,
-        function(){
-          // when done, add hash to url
-          // (default click behaviour)
-          window.location.hash = hash;
-        }
-      );
-    }
+    scrollTo();
 
   });
 });
+
--- openmeetings-db/js/reflow-skin.js
+++ openmeetings-db/js/reflow-skin.js
@@ -1,26 +1,95 @@
+"use strict";
+/*
+ * By Osvaldas Valutis, www.osvaldas.info Available for use under the MIT
+ * License
+ */
+(function($, window, document, undefined) {
+  $.fn.doubleTapToGo = function(params) {
+    if (!('ontouchstart' in window) && !navigator.msMaxTouchPoints
+        && !navigator.userAgent.toLowerCase().match(/windows phone os 7/i))
+      return false;
+
+    this.each(function() {
+      var curItem = false;
+
+      $(this).on('click', function(e) {
+        var item = $(this);
+        if (item[0] != curItem[0]) {
+          e.stopPropagation();
+          e.preventDefault();
+          curItem = item;
+          timeStamp = e.timeStamp;
+        }
+      });
+
+      $(document).on('click touchstart MSPointerDown', function(e) {
+        var resetItem = true, parents = $(e.target).parents();
+
+        for (var i = 0; i < parents.length; i++)
+          if (parents[i] == curItem[0]) {
+            resetItem = false;
+            break;
+          }
+
+        if (resetItem)
+          curItem = false;
+      });
+    });
+    return this;
+  };
+})(jQuery, window, document);
+
 // Additional skin Javascript
 // ++++++++++++++++++++++++++++++++++++++++++
+!function($) {
 
-!function ($) {
+  $(function() {
 
-	$(function(){
+    var $window = $(window)
 
-		var $window = $(window)
+    // Start carousel
+    $(function() {
+      $('.carousel').carousel();
+    });
 
-		// Start carousel
-		$(function() {
-			$('.carousel').carousel();
-		});
+    // activate syntax higlighting with highlight.js
+    // Note: only run if `hljs` exists
+    if (typeof hljs !== 'undefined') {
+      // classic encoding with <div class="source"><pre></pre></div>
+      // and HTML5 version with <pre><code></code></pre>
+      $('div.source pre, pre code').each(function(i, e) {
+        hljs.highlightBlock(e)
+      });
+    }
 
-		// activate syntax higlighting with highlight.js
-		// Note: only run if `hljs` exists
-		if (typeof hljs != 'undefined')
-		{
-			// classic encoding with <div class="source"><pre></pre></div>
-			// and HTML5 version with <pre><code></code></pre>
-			$('div.source pre, pre code').each(function(i, e) {hljs.highlightBlock(e)});
-		}
+    // toc aside bar
+    if ($('#toc-sidebar[data-spy=affix]').length) {
+      $('#toc-sidebar').affix({
+        offset : {
+          top : $('#toc-sidebar').offset().top,
+          bottom : ($('footer').outerHeight(true) + $('.subfooter').outerHeight(true) - 40)
+        // padding of footer.
+        }
+      });
+    }
 
-	})
+    function resizeTopNavBar() {
+      var navbar = $('#m-top-navbar');
+      var size = 0;
+      if (navbar.length) {
+        size = navbar.height() + 20; // normally 70
+      }
+      $('body').css('padding-top', size);
+    }
+
+    $(window).resize(resizeTopNavBar);
+    // initialize size on start up
+    resizeTopNavBar();
+
+    // prevents the browser from opening a URL but allows that if tapped once
+    // again in succession
+    $('.dropdown-submenu').doubleTapToGo();
+
+  });
 
 }(window.jQuery)
