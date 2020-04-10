969ff599f26d11a29adb2b62b731bc06945bbc4a
Sebastian Wagner <sebawagner@apache.org>
2020-04-11 10:25:12 +1200

    OPENMEETINGS-2248 Refresh and regenerate website and fix ci management link and page

--- js/reflow-skin.js
+++ js/reflow-skin.js
@@ -111,7 +111,7 @@ var mReflow = function () {
         size = navbar.outerHeight();
       }
       $('body').css('padding-top', size);
-      $('.toc-sidebar-fixed').css('top', size);
+      $('#m-toc-sidebar.toc-sidebar-fixed').css('top', size);
       $('#m-toc-topbar').css('top', size);
     }
     $window.resize(resizeTopNavBar);
@@ -157,7 +157,7 @@ var mReflow = function () {
       }
     }
 
-    $window.scroll( function () {
+    $window.scroll(function () {
       handle();
     });
 
@@ -165,7 +165,7 @@ var mReflow = function () {
       if ($body.hasClass('scrolltop-smooth-enabled')) {
         scrollTo(0, 0);
       } else {
-        $window.scrollTo(0, 0);
+        $window.scrollTop(0);
       }
     });
   }
@@ -203,6 +203,9 @@ var mReflow = function () {
   }
 
   function initAnchorJs() {
+    if (!$body.hasClass('anchorjs-enabled')) {
+      return;
+    }
     if (anchors && $body.hasClass('m-toc-sidebar-enabled') || $body.hasClass('m-toc-top-enabled')
       || $body.hasClass('m-sidenav-enabled')) {
       anchors.options = {
@@ -210,8 +213,8 @@ var mReflow = function () {
         class: 'fas fa-link',
         icon: ''
       };
-      anchors.add('.main-body h2, .main-body h3, .main-body h4, .main-body h5, .main-body h6');
-      $(".main-body h2, .main-body h3, .main-body h4, .main-body h5, .main-body h6").wrapInner("<div></div>");
+      anchors.add('.main-body h2:not(.no-anchor), .main-body h3:not(.no-anchor), .main-body h4:not(.no-anchor), .main-body h5:not(.no-anchor), .main-body h6:not(.no-anchor)');
+      $(".main-body h2:not(.no-anchor), .main-body h3:not(.no-anchor), .main-body h4:not(.no-anchor), .main-body h5:not(.no-anchor), .main-body h6:not(.no-anchor)").wrapInner("<div></div>");
     }
   }
 
@@ -258,18 +261,22 @@ var mReflow = function () {
     }
 
     $window.bind('hashchange', function (evt) {
-
       var originalEvt = evt.originalEvent;
+      // not load page if is identical
+      var oldURL;
+      var newURL;
       var identicalPage = false;
+
+      // do nothing if same url
       if (originalEvt) {
-        var oldURL = splitUrl(originalEvt.oldURL);
-        var newURL = splitUrl(originalEvt.newURL);
-        identicalPage = oldURL[0] === newURL[0];
+        oldURL = originalEvt.oldURL;
+        newURL = originalEvt.newURL;
+        if (oldURL === newURL) {
+          return;
+        }
       }
 
-      if (identicalPage) {
-        return;
-      }
+
 
       var item = null;
       var hash = window.location.hash;
@@ -293,16 +300,27 @@ var mReflow = function () {
       } else {
         item = $('.navside-menu a[slug-name$="' + section + '"]');
       }
-      // expand the parent of item if it is sub-section menu.
-      var collapsible = item.parents('ul.collapse');
-      if (collapsible.length > 0) {
-        collapsible.collapse('show');
-      }
-      var slugName = item.attr('slug-name');
-      window.location.hash = hashes(slugName, chapter);
-      var href = item.attr('href').substring(1);
-      loadFrame(href, slugName);
+      if (item.length) {
 
+        // expand the parent of item if it is sub-section menu.
+        var collapsible = item.parents('ul.collapse');
+        if (collapsible.length > 0) {
+          collapsible.collapse('show');
+        }
+        var slugName = item.attr('slug-name');
+        window.location.hash = hashes(slugName, chapter);
+
+        if (originalEvt) {
+          oldURL = splitUrl(originalEvt.oldURL);
+          newURL = splitUrl(originalEvt.newURL);
+          identicalPage = oldURL[0] === newURL[0];
+        }
+        if (identicalPage) {
+          return;
+        }
+        var href = item.attr('href').substring(1);
+        loadFrame(href, slugName);
+      }
     });
 
 
