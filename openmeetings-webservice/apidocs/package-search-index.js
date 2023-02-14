4813bae29a944986f38298e98b6a2a61b3da1b1e
Maxim Solodovnik <solomax666@gmail.com>
2023-02-14 09:57:44 +0700

    Site is update due to 7.0.0 release

--- openmeetings-webservice/apidocs/package-search-index.js
+++ openmeetings-webservice/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.apache.openmeetings.webservice"},{"l":"org.apache.openmeetings.webservice.error"},{"l":"org.apache.openmeetings.webservice.schema"},{"l":"org.apache.openmeetings.webservice.util"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.apache.openmeetings.webservice"},{"l":"org.apache.openmeetings.webservice.error"},{"l":"org.apache.openmeetings.webservice.schema"},{"l":"org.apache.openmeetings.webservice.util"}];updateSearchResults();
